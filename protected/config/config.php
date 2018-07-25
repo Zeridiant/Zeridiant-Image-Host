41fd129c70a17908e936c170dd752cdc0aa23399
Morgan <jmorganbullock@gmail.com>
2018-07-25 06:22:38 -0700

    Added file

---
+++ protected/config/config.php
@@ -0,0 +1,12 @@
+<?php
+define('RAW_IMAGE', true;
+define('RAW_IMAGE_LINK', false);
+define('IMAGE_EXTENSION', true);
+
+define('TWITTER_CARDS', false);
+
+define('IMAGESERVE_DIR', '');
+define('TWITTER_HANDLE', '@aerouk_');
+
+define('APP_NAME', 'Zeridiant-Image-Host');
+define('PASSKEY', getenv('PASSEY'));
